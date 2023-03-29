4a7f256d71a8ef40d0bc554d1f24c41968daf5dd
Antonio Sabino <aslimajr@outlook.com>
2023-03-29 00:14:01 -0300

    feat: criação do componente Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quos minima dolor delectus? Odio nulla officiis dicta deleniti, laborum eius soluta quasi at, illum dolorem id repellat, placeat facilis sint?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
